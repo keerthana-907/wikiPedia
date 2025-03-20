2549d0aa7905447227dbf286ae38aa0395b5a527
keerthana-907 <keerthanaparthiban20@gmail.com>
2025-03-20 16:41:37 +0530

    changes made in upload and contents

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1352879127"
+    serviceWorkerVersion: "1594815186"
   }
 });
